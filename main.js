d381cb7264cbeb1a84fbe96d8975943ff1903521
Carlos Domínguez <sharlybip@gmail.com>
2022-09-04 11:31:15 -0500

    agrgar el boton subir, o mas bien poenrlo visible

--- main.js
+++ main.js
@@ -182,3 +182,19 @@ file.addEventListener("change", () => {
   // Y a la fuente de la imagen le ponemos el objectURL
   showPrev.src = objectURL;
 });
+
+file.addEventListener("change", () => {
+  // Los archivos seleccionados, pueden ser muchos o uno
+  const archivos = file.files;
+  // Si no hay archivos salimos de la función y quitamos la imagen
+  if (!archivos || !archivos.length) {
+    showPrev.src = "";
+    return;
+  }
+  // Ahora tomamos el primer archivo, el cual vamos a previsualizar
+  const primerArchivo = archivos[0];
+  // Lo convertimos a un objeto de tipo objectURL
+  const objectURL = URL.createObjectURL(primerArchivo);
+  // Y a la fuente de la imagen le ponemos el objectURL
+  showPrev.src = objectURL;
+});
